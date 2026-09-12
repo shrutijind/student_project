ecc2f6d35072aa407593769f421be1ecba45caa7
shruti jindal <shrutijindal326@gmail.com>
2026-09-12 06:25:36 +0100

    Register Student and LeaveRequest in Django Admin

--- students/admin.py
+++ students/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
 
 # Register your models here.
+from django.contrib import admin
+from .models import Student, LeaveRequest
+
+admin.site.register(Student)
+admin.site.register(LeaveRequest)
