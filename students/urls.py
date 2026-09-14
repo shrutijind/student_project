139de71bac223ab5a934a8704e86a60418a3ab5a
shruti jindal <shrutijindal326@gmail.com>
2026-09-14 14:06:15 +0100

    Add root path routing to resolve 404 error on home URL

--- students/urls.py
+++ students/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    path('', views.student_login, name='login'),  # Root domain (/) goes straight to login
     path('login/', views.student_login, name='login'),
     path('logout/', views.student_logout, name='logout'),
     path('dashboard/', views.dashboard, name='dashboard'),
